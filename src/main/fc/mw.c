671a1e0f408df4bcace72d88a5de5af59b92c2e0
Martin Budden <mjbudden@gmail.com>
2016-11-28 21:01:29 +0000

    Prevent motor windup when on ground

--- src/main/fc/mw.c
+++ src/main/fc/mw.c
@@ -515,24 +515,12 @@ void processRx(uint32_t currentTime)
         failsafeUpdateState();
     }
 
-    throttleStatus_e throttleStatus = calculateThrottleStatus(&masterConfig.rxConfig, masterConfig.flight3DConfig.deadband3d_throttle);
+    const throttleStatus_e throttleStatus = calculateThrottleStatus(&masterConfig.rxConfig, masterConfig.flight3DConfig.deadband3d_throttle);
 
     if (isAirmodeActive() && ARMING_FLAG(ARMED)) {
         if (rcCommand[THROTTLE] >= masterConfig.rxConfig.airModeActivateThreshold) airmodeIsActivated = true; // Prevent Iterm from being reset
     } else {
         airmodeIsActivated = false;
-    }
-
-    /* In airmode Iterm should be prevented to grow when Low thottle and Roll + Pitch Centered.
-     This is needed to prevent Iterm winding on the ground, but keep full stabilisation on 0 throttle while in air */
-    if (throttleStatus == THROTTLE_LOW && !airmodeIsActivated) {
-        pidResetErrorGyroState();
-        if (currentProfile->pidProfile.pidAtMinThrottle)
-            pidStabilisationState(PID_STABILISATION_ON);
-        else
-            pidStabilisationState(PID_STABILISATION_OFF);
-    } else {
-        pidStabilisationState(PID_STABILISATION_ON);
     }
 
     // When armed and motors aren't spinning, do beeps and then disarm
@@ -582,6 +570,42 @@ void processRx(uint32_t currentTime)
     }
 
     processRcStickPositions(&masterConfig.rxConfig, throttleStatus, masterConfig.armingConfig.disarm_kill_switch);
+
+    /* In airmode Iterm should be prevented to grow when Low thottle and Roll + Pitch Centered.
+     This is needed to prevent Iterm winding on the ground, but keep full stabilisation on 0 throttle while in air */
+    if (throttleStatus == THROTTLE_LOW && !airmodeIsActivated) {
+        pidResetErrorGyroState();
+        if (currentProfile->pidProfile.pidAtMinThrottle)
+            pidSetStabilisationState(PID_STABILISATION_ON);
+        else
+            pidSetStabilisationState(PID_STABILISATION_OFF);
+    } else {
+        pidSetStabilisationState(PID_STABILISATION_ON);
+    }
+
+    // Prevent motor windup on when on ground
+    // rcCommand in the range -500 to 500 at this point
+    // create a center band slightly wider than the deadband
+    const int centerBand = masterConfig.rcControlsConfig.deadband + 4;
+    const int yawCenterBand = masterConfig.rcControlsConfig.yaw_deadband + 4;
+    if (rcCommand[THROTTLE] > 1200
+            || rcCommand[ROLL]  < -centerBand || rcCommand[ROLL]  > centerBand
+            || rcCommand[PITCH] < -centerBand || rcCommand[PITCH] > centerBand
+            || rcCommand[YAW]   < -yawCenterBand || rcCommand[YAW]   > yawCenterBand) {
+        // do nothing
+    } else {
+        // all sticks centered and throttle low
+#define COS_5_DEGREES 0.996194698091746f
+        if (getCosTiltAngle() > COS_5_DEGREES) {
+            // we are nearly level
+            const int32_t accZ = accSmooth[Z] * 100 / acc.acc_1G;
+            if ((accZ > 80) && (accZ < 110)) {
+                // accelerometer  Z readings is in the range 0.80g - 1.10g, so we are not in free fall
+                // so don't allow ITerm to wind-up
+                pidSetStabilisationState(PID_STABILISATION_ZERO_ITERM);
+            }
+        }
+    }
 
     if (feature(FEATURE_INFLIGHT_ACC_CAL)) {
         updateInflightCalibrationState();
