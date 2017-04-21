49363ef549ac867465e26e034879d444f46e8f65
Michael Keller <github@ike.ch>
2017-04-21 19:05:37 +1200

    Added Matek F411 / F722 boards.

---
+++ src/main/target/MATEKF722/target.c
@@ -0,0 +1,44 @@
+/*
+* This file is part of Cleanflight.
+*
+* Cleanflight is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* Cleanflight is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with Cleanflight.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include <stdint.h>
+
+#include <platform.h>
+#include "drivers/io.h"
+
+#include "drivers/dma.h"
+#include "drivers/timer.h"
+#include "drivers/timer_def.h"
+
+const timerHardware_t timerHardware[USABLE_TIMER_CHANNEL_COUNT] = {
+	DEF_TIM(TIM5, CH4, PA3,  TIM_USE_PPM,   TIMER_INPUT_ENABLED,  0),  	// PPM IN - DMA1_Stream1
+	
+	DEF_TIM(TIM3, CH1, PC6,  TIM_USE_MOTOR, TIMER_OUTPUT_ENABLED, 0), 	// S1_OUT - DMA1_Stream4
+	DEF_TIM(TIM8, CH2, PC7,  TIM_USE_MOTOR, TIMER_OUTPUT_ENABLED, 1), 	// S2_OUT - DMA2_Stream2
+	DEF_TIM(TIM8, CH3, PC8,  TIM_USE_MOTOR, TIMER_OUTPUT_ENABLED, 0),   // S3_OUT - DMA2_Stream4
+	DEF_TIM(TIM8, CH4, PC9,  TIM_USE_MOTOR, TIMER_OUTPUT_ENABLED, 0), 	// S4_OUT - DMA2_Stream7
+	
+	DEF_TIM(TIM2, CH1, PA15, TIM_USE_MOTOR, TIMER_OUTPUT_ENABLED, 0), 	// S5_OUT - DMA1_Stream5
+	DEF_TIM(TIM1, CH1, PA8,  TIM_USE_MOTOR, TIMER_OUTPUT_ENABLED, 0), 	// S6_OUT - DMA2_Stream6
+
+#ifdef MATEKF722_V1_2
+    DEF_TIM(TIM4, CH3, PB8,  TIM_USE_LED,   TIMER_OUTPUT_ENABLED, 0)    // LED - DMA1_Stream7
+#else
+    DEF_TIM(TIM4, CH1, PB6,  TIM_USE_LED,   TIMER_OUTPUT_ENABLED, 0)    // LED - DMA1_Stream0
+#endif
+};
+
