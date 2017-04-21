527b801f974758c086a2b9fbe6fc77331401a4df
Michael Keller <github@ike.ch>
2017-04-21 19:05:37 +1200

    Added Matek F411 / F722 boards.

---
+++ src/main/target/MATEKF722/target.mk
@@ -0,0 +1,9 @@
+F7X2RE_TARGETS += $(TARGET)
+FEATURES       += VCP SDCARD
+
+TARGET_SRC = \
+            drivers/accgyro/accgyro_spi_mpu6500.c \
+            drivers/accgyro/accgyro_mpu6500.c \
+            drivers/max7456.c \
+            drivers/light_ws2811strip.c \
+            drivers/light_ws2811strip_hal.c
