6a3c92e041216800697fcae81c8019674a640c19
mikeller <github@ike.ch>
2018-06-23 11:15:49 +1200

    Updated target definition.

--- src/main/target/DYSF4PROV3/target.c
+++ src/main/target/DYSF4PROV3/target.c
@@ -34,10 +34,10 @@ const timerHardware_t timerHardware[USABLE_TIMER_CHANNEL_COUNT] = {
     DEF_TIM(TIM3,  CH2, PC7,  TIM_USE_MOTOR, 0, 0), // S2 (UP: 1, 2) (1, 5)
     DEF_TIM(TIM3,  CH3, PC8,  TIM_USE_MOTOR, 0, 0), // S3 (UP: 1, 2) (1, 7)
     DEF_TIM(TIM3,  CH4, PC9,  TIM_USE_MOTOR, 0, 0), // S4 (UP: 1, 2) (1, 2)
-    DEF_TIM(TIM2,  CH3, PB2,  TIM_USE_MOTOR, 0, 0), // S5 (UP: 1, 1) (1, 1)
+    DEF_TIM(TIM2,  CH3, PB2,  TIM_USE_MOTOR | TIM_USE_LED, 0, 0), // S5 (UP: 1, 1) (1, 1)
     DEF_TIM(TIM2,  CH1, PB10, TIM_USE_MOTOR, 0, 0), // S6 (UP: 1, 1) (1, 3)
 
-    DEF_TIM(TIM4,  CH4, PB9,  TIM_USE_LED,   0, 0), // LED STRIP (no DMA, won't work)
+//    DEF_TIM(TIM4,  CH4, PB9,  TIM_USE_LED,   0, 0), // LED STRIP (no DMA, won't work)
 //    DEF_TIM(TIM4,  CH4, PB9,  TIM_USE_PWM,   0, 0), // CAM (no DMA)
-    DEF_TIM(TIM1,  CH1, PA8,  TIM_USE_ANY,   0, 0), // LED STRIP (2, 1)
+//    DEF_TIM(TIM1,  CH1, PA8,  TIM_USE_ANY,   0, 0), // LED STRIP (2, 1)
 };
